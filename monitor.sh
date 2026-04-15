595dd0ca9d82285e5668d02d17651b7c5242e4e9
AaronMadrigal2502 <aarondanielmadrigalmarin@gmail.com>
2026-04-14 22:32:48 -0600

    nuevo intento

--- monitor.sh
+++ monitor.sh
@@ -10,7 +10,7 @@ fi
 CMD="$1"
 INTERVAL="${2:-2}"
 
-if ! [[ "$INTERVAL" =~ ^[0-9]+$ ]] || ["$INTERVAL" -le 0 ]; then
+if ! [[ "$INTERVAL" =~ ^[0-9]+$ ]] || [ "$INTERVAL" -le 0 ]; then
 	echo "Error: el intervalo debe ser un entero positivo."
 	echo "Uso: $0 \"comando [args]\" [intervalo_segundos]"
 	exit 1
